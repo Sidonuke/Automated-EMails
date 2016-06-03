6875ff6b1ebe7a7dd363947df43781cc8e4342b2
Sidonuke <Sidonuke.coding@gmail.com>
2016-06-03 11:17:50 -0500

    Update copy.py

--- copy.py
+++ copy.py
@@ -1,6 +1,6 @@
 import mysql.connector #Helpful to connect to the DB
 
-cnx = mysql.connector.connect(user='sidonukenet2', password='Sidonukeg5', host='mysql.sidonuke.net', database='sidonuke_net_2') #SQL Info
+cnx = mysql.connector.connect(user='USERNAME', password='PASSWORD', host='SQL SERVER', database='DATABASE') #SQL Info
 cursor = cnx.cursor(buffered=True)
 
 cursor.execute ("INSERT INTO UnlocksDB SELECT * FROM Unlocks")
