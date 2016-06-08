c12b54e24cb82633c97043e2e0ada4b3e15139ca
Sidonuke <Sidonuke.coding@gmail.com>
2016-06-08 11:04:49 -0500

    Update Email.py
    - Added if no table data
    - Fixed list brackets showing in email
    - Fixed new line issue to remove JSON

--- Email.py
+++ Email.py
@@ -1,46 +1,39 @@
-#!/usr/bin/python
 from smtplib import SMTP #Email orders
 import mysql.connector #Helpful to connect to the DB
 import datetime #Current date for email
-import json #Used below simply for indenting ;)
 
-cnx = mysql.connector.connect(user='USERNAME', password='PASSWORD', host='HOSTNAME', database='DATABASE') #SQL Info
-cursor = cnx.cursor()
-
-query = ("SELECT Time, Name, Flight, Reason, Done FROM Unlocks ") #Only the needed SQL Fields
-
+sequel = mysql.connector.connect(user='USERNAME', password='PASSWORD', host='SQLADDRESS', database='DATABASENAME') #SQL Info
+cursor = sequel.cursor()
+query = ('SELECT Time, Name, Flight, Reason, Done FROM Unlocks ') #Only the needed SQL Fields
 cursor.execute(query)
-
-squares = list() #Don't ask why it's named squares.
-for (Time, Name, Flight, Reason, Done) in cursor:
-    squares.append("At {}, {} called to unlock {} due to {}, This was completed at {}".format(
-    Time, Name, Flight, Reason, Done)) #Add each list item depending on values in SQL DB
 
 date = datetime.datetime.now() #Date in SUBJECT line below.
 
+body = list() #Our body for email
+for (Time, Name, Flight, Reason, Done) in cursor:
+    body.append('At {}, {} called to unlock {}, due to {} This was completed at {}.'.format(
+    Time, Name, Flight, Reason, Done)) #Add each list item depending on values in SQL DB
+if not body:
+    body.append('No unlocks were needed for ' + date.strftime('%m, %d, %Y')) #If no data in the tables above then inserts this as the email body.
+
+body = '\n'.join(body) #New line for each row in the table Unlocks
+body = (str(body).replace('[','').replace(']','')) #Pesky brackets would not go away!
+
 smtp = SMTP()
-smtp.connect('HOSTNAME', SMTPPORT) #Our Email Server
-smtp.login('USER@EMAIL.COM', 'PASSWORD') #Email Credentials. 
-
-FROM = 'WHO IS SENDING THE EMAIL (USER@EMAIL.COM)'
-
-TO = ["RECEIVER (BOSS@EMAIL.COM)"] # must be a list
-
-SUBJECT = "Flight unlocks for" + " " + date.strftime("%m, %d, %Y")
-
-TEXT = ('Good Evening All,'+'\n'+'\n' + json.dumps(squares, indent = 0)) #Formatting and indenting
-
-message = """\
+smtp.connect('EMAILSERVER', SMTPPORT) #Our Email Server
+smtp.login('USERNAME', 'EMAILPASS') #Email Credentials.
+FROM = 'YOU@EMAIL.com'
+TO = ['USER@EMAIL.COM', 'USER1@EMAIL.COM', 'USER2@EMAIL.COM'] # must be a list (seperated by a comma)
+SUBJECT = 'Flight unlocks for' + ' ' + date.strftime('%m, %d, %Y')
+TEXT = ('Good Evening All,'+'\n'+'\n' + body)#Formatting and indenting
+message = '''\
 From: %s
 To: %s
 Subject: %s
-
 %s
-""" % (FROM, ", ".join(TO), SUBJECT, TEXT)
-
-# Send the mail
-
+''' % (FROM, ', '.join(TO), SUBJECT, TEXT)
 smtp.sendmail(FROM, TO, message)
+
 smtp.quit()
 cursor.close()
-cnx.close()
+sequel.close()
